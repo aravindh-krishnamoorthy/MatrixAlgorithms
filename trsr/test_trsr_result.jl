581ede3014d9f7d232f19b31099e3156d98aa614
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2026-04-13 14:11:54 +0200

    Updates

--- trsr/test_trsr_result.jl
+++ trsr/test_trsr_result.jl
@@ -4,46 +4,25 @@ using BenchmarkTools
 for iter = 1:1000
     println("$iter/1000...")
     for N in [8, 64, 1000]
-            # Real trsr!
-            X = randn(N,N)
-            X1 = MatrixAlgorithms.sqrtm(X)
-            @assert X1*X1 ≈ X
-            X1 = MatrixAlgorithms.sqrtm(X+X')
-            @assert X1*X1 ≈ X+X'
-            X1 = MatrixAlgorithms.sqrtm(X+N*I(N))
-            @assert X1*X1 ≈ X+N*I(N)
-            X1 = MatrixAlgorithms.sqrtm(X+X'+N*I(N))
-            @assert X1*X1 ≈ X+X'+N*I(N)
-            # Real trsrf!
-            X = randn(N,N)
-            X1 = MatrixAlgorithms.sqrtm(X, MatrixAlgorithms.trsrf!)
-            @assert X1*X1 ≈ X
-            X1 = MatrixAlgorithms.sqrtm(X+X', MatrixAlgorithms.trsrf!)
-            @assert X1*X1 ≈ X+X'
-            X1 = MatrixAlgorithms.sqrtm(X+N*I(N), MatrixAlgorithms.trsrf!)
-            @assert X1*X1 ≈ X+N*I(N)
-            X1 = MatrixAlgorithms.sqrtm(X+X'+N*I(N), MatrixAlgorithms.trsrf!)
-            @assert X1*X1 ≈ X+X'+N*I(N)
-            # Complex trsr!
-            X = complex.(randn(N,N),randn(N,N))
-            X1 = MatrixAlgorithms.sqrtm(X)
-            @assert X1*X1 ≈ X
-            X1 = MatrixAlgorithms.sqrtm(X+X')
-            @assert X1*X1 ≈ X+X'
-            X1 = MatrixAlgorithms.sqrtm(X+N*I(N))
-            @assert X1*X1 ≈ X+N*I(N)
-            X1 = MatrixAlgorithms.sqrtm(X+X'+N*I(N))
-            @assert X1*X1 ≈ X+X'+N*I(N)
-            # Complex trsrf!
-            X = complex.(randn(N,N),randn(N,N))
-            X1 = MatrixAlgorithms.sqrtm(X, MatrixAlgorithms.trsrf!)
-            @assert X1*X1 ≈ X
-            X1 = MatrixAlgorithms.sqrtm(X+X', MatrixAlgorithms.trsrf!)
-            @assert X1*X1 ≈ X+X'
-            X1 = MatrixAlgorithms.sqrtm(X+N*I(N), MatrixAlgorithms.trsrf!)
-            @assert X1*X1 ≈ X+N*I(N)
-            X1 = MatrixAlgorithms.sqrtm(X+X'+N*I(N), MatrixAlgorithms.trsrf!)
-            @assert X1*X1 ≈ X+X'+N*I(N)
-        end
+        # Real trsr!
+        X = randn(N,N)
+        X1 = sqrtm(X)
+        @assert X1*X1 ≈ X
+        X1 = sqrtm(X+X')
+        @assert X1*X1 ≈ X+X'
+        X1 = sqrtm(X+N*I(N))
+        @assert X1*X1 ≈ X+N*I(N)
+        X1 = sqrtm(X+X'+N*I(N))
+        @assert X1*X1 ≈ X+X'+N*I(N)
+        # Complex trsr!
+        X = complex.(randn(N,N),randn(N,N))
+        X1 = sqrtm(X)
+        @assert X1*X1 ≈ X
+        X1 = sqrtm(X+X')
+        @assert X1*X1 ≈ X+X'
+        X1 = sqrtm(X+N*I(N))
+        @assert X1*X1 ≈ X+N*I(N)
+        X1 = sqrtm(X+X'+N*I(N))
+        @assert X1*X1 ≈ X+X'+N*I(N)
     end
 end
